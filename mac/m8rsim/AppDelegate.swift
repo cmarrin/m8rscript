d837ddc732acebbfa08f94dfd128b60849e73d50
Chris Marrin <cmarrin@apple.com>
2016-06-23 13:57:54 -0700

    Added a m8rsim target to start building a simulator app

---
+++ mac/m8rsim/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  m8rsim
+//
+//  Created by Chris Marrin on 6/23/16.
+//  Copyright © 2016 MarrinTech. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
