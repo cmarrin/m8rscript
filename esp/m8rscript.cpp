6218239d3fdf9063949c54b39a05f01850e3375e
Chris Marrin <chris@marrin.com>
2016-06-28 06:55:23 -0700

    Created a minimal file with everything needed to link m8rscript. Cobbled together from the Arduino ESP8266 Cores library. Got it down to ESP.h and ESP.cpp, along with the empty user_config.h. Saves 6k of Flash and 5k of precious ram. Everything is stubbed out, most importantly malloc and free, so they will increase the size when added back. But I wanted to get rid of all the extraneous string functions (especially printf) which I won't be using, as well as other Arduino compatibility overhead

--- esp/m8rscript.cpp
+++ esp/m8rscript.cpp
@@ -8,19 +8,16 @@ public:
     virtual void print(const char*) const override { }
 };
 
-extern "C" void setup()
+extern "C" void user_init()
 {
+    MyPrinter printer;
     // put your setup code here, to run once:
     m8r::FileStream istream("");
-    m8r::Parser parser(&istream, MyPrinter);
-    m8r::ExecutionUnit eu(MyPrinter);
+    m8r::Parser parser(&istream, &printer);
+    m8r::ExecutionUnit eu(&printer);
     eu.generateCodeString(parser.program()).c_str();
     eu.run(parser.program());
 
     //m8r::Program program;
-    //eu.run(&program, print);
+    //eu.run(&program, print);    
 }
-
-extern "C" void loop() { }
-
-//extern "C" void user_init() { }
