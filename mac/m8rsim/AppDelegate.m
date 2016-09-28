047c21ed60debe4009b298d845e5e0b11e8b957c
Chris Marrin <cmarrin@apple.com>
2016-09-28 16:33:57 -0700

    Revert "Converted AppDelegate to Swift"
    This reverts commit a233e0bebdcbf6067ce4c8467648be74a6a1f161.

---
+++ mac/m8rsim/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  m8rsim
+//
+//  Created by Chris Marrin on 6/24/16.
+//  Copyright © 2016 Chris Marrin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    NSLog(@"finish launch");
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    NSLog(@"term");
+}
+
+@end
