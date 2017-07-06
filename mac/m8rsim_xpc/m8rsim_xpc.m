c602aa0efe9db2f085326935b571d215e2e9edf2
Chris Marrin <chris@marrin.com>
2017-07-06 07:18:47 -0700

    Added XPC target for local m8r device

---
+++ mac/m8rsim_xpc/m8rsim_xpc.m
@@ -0,0 +1,19 @@
+//
+//  m8rsim_xpc.m
+//  m8rsim_xpc
+//
+//  Created by Chris Marrin on 7/4/17.
+//  Copyright © 2017 MarrinTech. All rights reserved.
+//
+
+#import "m8rsim_xpc.h"
+
+@implementation m8rsim_xpc
+
+// This implements the example protocol. Replace the body of this class with the implementation of this service's protocol.
+- (void)upperCaseString:(NSString *)aString withReply:(void (^)(NSString *))reply {
+    NSString *response = [aString uppercaseString];
+    reply(response);
+}
+
+@end
