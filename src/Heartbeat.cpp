ff035c8d58f27d626424e211ae088d7e28ff36c8
Chris Marrin <chris@marrin.com>
2019-10-05 23:30:23 -0700

    Added Heartbeat class - just a skeleton for now

---
+++ src/Heartbeat.cpp
@@ -0,0 +1,9 @@
+//
+//  Heartbeat.cpp
+//  libm8rscript
+//
+//  Created by Chris Marrin on 10/5/19.
+//  Copyright © 2019 MarrinTech. All rights reserved.
+//
+
+#include "Heartbeat.hpp"
---
+++ src/Heartbeat.h
@@ -0,0 +1,14 @@
+//
+//  Heartbeat.hpp
+//  libm8rscript
+//
+//  Created by Chris Marrin on 10/5/19.
+//  Copyright © 2019 MarrinTech. All rights reserved.
+//
+
+#ifndef Heartbeat_hpp
+#define Heartbeat_hpp
+
+#include <stdio.h>
+
+#endif /* Heartbeat_hpp */
