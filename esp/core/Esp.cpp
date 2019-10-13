379d67ffb18f300b81792444184cc0d549bf1c96
Chris Marrin <chris@marrin.com>
2019-10-13 13:26:38 -0700

    Got ESP build working

--- esp/core/Esp.cpp
+++ esp/core/Esp.cpp
@@ -365,13 +365,13 @@ void writeUserData()
     _gUserData.magic[1] = '8';
     _gUserData.magic[2] = 'r';
     _gUserData.magic[3] = 's';
-    std::shared_ptr<m8r::File> file = m8r::system()->fileSystem()->open(UserDataFilename, "w");
+    std::shared_ptr<m8r::File> file = m8r::system()->fileSystem()->open(UserDataFilename, m8r::FS::FileOpenMode::Write);
     int32_t count = file->write(reinterpret_cast<const char*>(&_gUserData), sizeof(UserSaveData));
 }
 
 void getUserData()
 {
-    std::shared_ptr<m8r::File> file = m8r::system()->fileSystem()->open(UserDataFilename, "r");
+    std::shared_ptr<m8r::File> file = m8r::system()->fileSystem()->open(UserDataFilename, m8r::FS::FileOpenMode::Read);
     int32_t count = file->read(reinterpret_cast<char*>(&_gUserData), sizeof(UserSaveData));
 
     if (_gUserData.magic[0] != 'm' || _gUserData.magic[1] != '8' || 
