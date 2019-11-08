6975b3b17e59a4864c51bdc5e53b5b72607874b4
Chris Marrin <chris@marrin.com>
2019-11-07 22:14:29 -0800

    Added some text with an idea for a memory allocator

--- src/Mallocator.h
+++ src/Mallocator.h
@@ -22,6 +22,33 @@ namespace m8r {
 //
 //  C++11 Allocator which tracks allocations
 //
+//  Design of the allocator
+//
+//  This allocator has zero overhead for allocated storage and allocates
+//  on 4 byte boundariess. This is problematic on Mac, which expects to
+//  align pointers to 8 byte boundaries. So some adjustment may be needed
+//  But in a low memory envoronment (~50KB) it's crucial to save space, even
+//  if it means taking more time to, for instance, free a block.
+//
+//  Memory is alloted on 4 byte boundaries. Memory is tracked through block
+//  id, which is a uint16_t. With 4 bytes blocks, that allows 256KB of memory.
+//  For larger memory sizes the block size can be increased. 8 byte blocks
+//  would allow 512KB and 16 byte blocks would allow 1MB. Larger than that
+//  maybe this is not the right allocator. But for the ESP8266 at around 
+//  50KB of usable RAM and the ESP32 at around 400KB, this allocator should
+//  do fine.
+//
+//  The block id allows blocks to be on 4 byte boundaries, but blocks can be
+//  larger than that. Allocated blocks can be as large as available memory.
+
+//  The free list uses the first 4 bytes of a block, 2 bytes for a block id
+//  of the next free block, and a 2 byte block size. Actually the size byte
+//  may contain some flags, so the maximum free block size may be smaller.
+//  If so, we will have to allow for contiguous free blocks. While possible
+//  that complicates the design and require coalescing free blocks. So maybe
+//  we can limit the maximum allocated block size to 14 bits, 16K blocks or 
+//  64KB. That gives 2 bits of flags, if needed.
+//
 //////////////////////////////////////////////////////////////////////////////
 
 enum class MemoryType {
