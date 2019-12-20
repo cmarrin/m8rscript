c135e0f3217f1bc64e2056c0bf11bb03ea691572
Chris Marrin <chris@marrin.com>
2019-12-20 08:21:32 -0800

    Got rid of filesystem tests and added the rest of the init sequence.
    Asserting in some spiffs code, probably during mount

--- esp/main/m8rscript.cpp
+++ esp/main/m8rscript.cpp
@@ -47,96 +47,14 @@ void m8r::heapInfo(void*& start, uint32_t& size)
 
 extern "C" void app_main()
 {
-    sleep(2);
-    printf("******** registering SPIFFS\n");
-    esp_err_t ret = esp_vfs_spiffs_register(&conf);
-
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            printf("Failed to mount or format filesystem\n");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            printf("Failed to find SPIFFS partition\n");
-        } else {
-            printf("Failed to initialize SPIFFS (%s)\n", esp_err_to_name(ret));
-        }
-        return;
-    }
-    printf("******** registration succeeded\n");
-
-    size_t total = 0, used = 0;
-    ret = esp_spiffs_info(NULL, &total, &used);
-    if (ret != ESP_OK) {
-        printf("Failed to get SPIFFS partition information (%s)\n", esp_err_to_name(ret));
-    } else {
-        printf("Partition size: total: %d, used: %d\n", total, used);
-    }
-    
-    printf("******** creating /spiffs/hello.txt\n");
-    int fd = open("/spiffs/hello.txt", O_WRONLY | O_CREAT | O_EXCL);
-    if (fd >= 0) {
-        printf("******** Opened file for write: %d\n", fd);
-        
-        ssize_t result = write(fd, "Hello World", 11);
-        printf("******** Wrote to file: result=%d\n", static_cast<int>(result));
-        close(fd);
-
-        // Check if destination file exists before renaming
-        printf("******** checking for existance of /spiffs/foo.txt\n");
-        struct stat st;
-        if (stat("/spiffs/foo.txt", &st) == 0) {
-            // Delete it if it exists
-            printf("******** file exists deleting\n");
-            unlink("/spiffs/foo.txt");
-        }
-
-        // Rename original file
-        printf("******** Renaming file\n");
-        if (rename("/spiffs/hello.txt", "/spiffs/foo.txt") != 0) {
-            printf("******** Rename failed\n");
-            return;
-        }
-
-        printf("******** opening /spiffs/foo.txt for read\n");
-        int fd = open("/spiffs/foo.txt", O_RDONLY);
-        if (fd >= 0) {
-            printf("******** Opened file for read: %d\n", fd);
-            
-            char buf[12];
-            ssize_t result = read(fd, buf, 11);
-            buf[11] = '\0';
-            printf("******** Read to file: result=%d, '%s'\n", static_cast<int>(result), buf);
-            close(fd);
-        }
-    } else {
-        printf("******* File open failed:%d\n", errno);
-    }
-
-
-
-
-
-
     printf("\n*** m8rscript v%d.%d - %s\n\n", m8r::MajorVersion, m8r::MinorVersion, __TIMESTAMP__);
-
-    m8r::ROMString s = ROMSTR("*** HELLO ***\n");
-    printf(s.value());
-    
-    m8r::Mad<char> chars = m8r::Mad<char>::create(22);
-    strcpy(chars.get(), "It's Mad I tell you!\n");
-    printf(chars.get());
-    
-    char* newstr = new char[22];
-    strcpy(newstr, "Simply Mad!\n");
-    printf(newstr);
-    
     const m8r::MemoryInfo& info = m8r::Mallocator::shared()->memoryInfo();
-
     m8r::system()->printf(ROMSTR("Total heap: %d, free heap: %d\n"), info.heapSizeInBlocks * info.blockSize, info.freeSizeInBlocks * info.blockSize);
     
     m8r::Application application(23);
     printf("after application ctor\n");
-    //m8r::Application::mountFileSystem();
-    //printf("after application mountFileSystem\n");
-    //application.runLoop();
-    //printf("after application runLoop\n");
+    m8r::Application::mountFileSystem();
+    printf("after application mountFileSystem\n");
+    application.runLoop();
+    printf("after application runLoop\n");
 }
