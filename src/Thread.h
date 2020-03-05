7b5c3070c4fb6ad796714ac8df80bbfca82927e6
Chris Marrin <chris@marrin.com>
2020-03-05 15:01:04 -0800

    Made Thread work without crashing
    This is a fix that was in the NoTaskManager branch. Bringing it forward

--- src/Thread.h
+++ src/Thread.h
@@ -16,34 +16,64 @@ namespace m8r {
 
 class Thread {
 public:
-    Thread() { }
+    Thread() = default;
 
     template< class Function, class... Args > 
     explicit Thread( Function&& f, Args&&... args )
     {
-        _lambda = std::bind(std::forward<Function>(f), std::forward<Args>(args)...);
-        pthread_create(&_thread, nullptr, threadFunc, this);
+        auto storage = new ThreadStorage(std::move(std::bind(std::forward<Function>(f), std::forward<Args>(args)...)));
+        pthread_create(&_thread, nullptr, threadFunc, storage);
     }
     
-    Thread(Thread&& other) { _thread = other._thread; other._thread = pthread_t(); }
+    Thread(Thread&& other) { swap(other); }
+    Thread(const Thread&) = delete;
     Thread(Thread&) = delete;
         
-    ~Thread() { }
-    
+    ~Thread()
+    {
+        if (joinable()) {
+            join();
+        }
+    }
+
+    Thread& operator=(const Thread&) = delete;
+
+    Thread& operator=(Thread&& other)
+    {
+        if (joinable()) {
+            terminate();
+        }
+        swap(other);
+        return *this;
+    }
+
+    void swap(Thread& other) { std::swap(_thread, other._thread); }
+        
     void join() { pthread_join(_thread, nullptr); }
     void detach() { pthread_detach(_thread); }
+    void terminate() { pthread_cancel(_thread); }
+    bool joinable() { return _running; }
 
 private:
+    struct ThreadStorage
+    {
+        ThreadStorage(std::function<void()>&& func)
+            : _lambda(std::move(func))
+        { }
+            
+        std::function<void()> _lambda;
+    };
+    
     static void* threadFunc(void* data)
     {
-        Thread* t = reinterpret_cast<Thread*>(data);
+        auto t = reinterpret_cast<ThreadStorage*>(data);
         t->_lambda();
+        delete t;
         return nullptr;
     }
     
-    std::function<void()> _lambda;
     pthread_t _thread = pthread_t();
-    
+    bool _running = false;    
 };
 
 }
