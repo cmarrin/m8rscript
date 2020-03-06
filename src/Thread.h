b56d5dbdee4d86cf93fb26cb32ece4fba71efbc9
Chris Marrin <chris@marrin.com>
2020-03-05 16:17:54 -0800

    Added this_thread functions to Thread

--- src/Thread.h
+++ src/Thread.h
@@ -9,6 +9,8 @@
 
 #pragma once
 
+#include "SystemTime.h"
+
 #include <pthread.h>
 #include <functional>
 
@@ -17,11 +19,13 @@ namespace m8r {
 class Thread {
 public:
     Thread() = default;
+    
+    using id = pthread_t;
 
     template< class Function, class... Args > 
     explicit Thread( Function&& f, Args&&... args )
     {
-        auto storage = new ThreadStorage(std::move(std::bind(std::forward<Function>(f), std::forward<Args>(args)...)));
+        auto storage = new ThreadStorage(this, std::move(std::bind(std::forward<Function>(f), std::forward<Args>(args)...)));
         pthread_create(&_thread, nullptr, threadFunc, storage);
     }
     
@@ -35,6 +39,8 @@ public:
             join();
         }
     }
+    
+    id get_id() const { return _thread; }
 
     Thread& operator=(const Thread&) = delete;
 
@@ -52,28 +58,38 @@ public:
     void join() { pthread_join(_thread, nullptr); }
     void detach() { pthread_detach(_thread); }
     void terminate() { pthread_cancel(_thread); }
-    bool joinable() { return _running; }
+    bool joinable() { return _thread != pthread_t(); }
 
 private:
     struct ThreadStorage
     {
-        ThreadStorage(std::function<void()>&& func)
-            : _lambda(std::move(func))
+        ThreadStorage(Thread* thread, std::function<void()>&& func)
+            : _thread(thread)
+            , _lambda(std::move(func))
         { }
             
         std::function<void()> _lambda;
+        Thread* _thread;
     };
     
     static void* threadFunc(void* data)
     {
         auto t = reinterpret_cast<ThreadStorage*>(data);
         t->_lambda();
+        t->_thread->_thread = pthread_t();
         delete t;
         return nullptr;
     }
     
     pthread_t _thread = pthread_t();
-    bool _running = false;    
 };
+
+namespace this_thread {
+    
+    inline Thread::id get_id() { return pthread_self(); }
+    inline void yield() { Duration d = 1_us; d.sleep(); }
+    inline void sleep_for(Duration d) { d.sleep(); }
+    inline void sleep_until(Time t) { sleep_for(t - Time::now()); }
+}
 
 }
