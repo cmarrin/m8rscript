fee471778b686e9d3dc70ce7ce8853004e8f7b55
Chris Marrin <chris@marrin.com>
2020-04-01 08:24:17 -0700

    WIP - Getting Arduino main.cpp to work

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
-// #include <Arduino.h>
+#include <Arduino.h>
+//#include "Application.h"
+#include <chrono>
 
-// void setup() {
-//   // put your setup code here, to run once:
-// }
+bool ready = false;
+bool state = false;
 
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
+void ICACHE_RAM_ATTR onTimerISR(){
+    ready = true;
+    timer1_write(5000000);
+}
+
+void setup()
+{
+    Serial.begin(74880);
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    //m8r::Application application(23);
+
+    //m8r::system()->printf(ROMString("\n*** m8rscript v%d.%d - %s\n\n"), m8r::MajorVersion, m8r::MinorVersion, __TIMESTAMP__);
+    //m8r::system()->printf(ROMString("Free heap: %d\n"), m8r::Mallocator::shared()->freeSize());
+    //application.runLoop();
+
+    timer1_attachInterrupt(onTimerISR);
+    timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
+    timer1_write(5000000); //120000 us
+}
+
+void loop()
+{
+    if (ready) {
+        ready = false;
+        state = !state;
+        digitalWrite(LED_BUILTIN, state ? HIGH : LOW);
+
+        if (state) {
+            std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+            uint64_t t = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count();
+            Serial.printf("******** now = %d\n", (int) t);
+        }
+    }
+}
