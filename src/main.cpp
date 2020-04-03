23ef2a57e786f62f7604ddec213da72908324872
Chris Marrin <chris@marrin.com>
2020-04-03 16:38:46 -0700

    Enabled all the m8rscript logic in main.cpp for ESP

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
-#include <Arduino.h>
-//#include "Application.h"
+#include "Application.h"
 #include <chrono>
 
 bool ready = false;
@@ -10,16 +9,17 @@ void ICACHE_RAM_ATTR onTimerISR(){
     timer1_write(5000000);
 }
 
+m8r::Application* _application = nullptr;
+
 void setup()
 {
     Serial.begin(74880);
     pinMode(LED_BUILTIN, OUTPUT);
 
-    //m8r::Application application(23);
+    _application = new m8r::Application(23);
 
-    //m8r::system()->printf(ROMString("\n*** m8rscript v%d.%d - %s\n\n"), m8r::MajorVersion, m8r::MinorVersion, __TIMESTAMP__);
-    //m8r::system()->printf(ROMString("Free heap: %d\n"), m8r::Mallocator::shared()->freeSize());
-    //application.runLoop();
+    m8r::system()->printf(m8r::ROMString("\n*** m8rscript v%d.%d - %s\n\n"), m8r::MajorVersion, m8r::MinorVersion, __TIMESTAMP__);
+    m8r::system()->printf(m8r::ROMString("Free heap: %d\n"), m8r::Mallocator::shared()->freeSize());
 
     timer1_attachInterrupt(onTimerISR);
     timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
@@ -28,15 +28,17 @@ void setup()
 
 void loop()
 {
-    if (ready) {
-        ready = false;
-        state = !state;
-        digitalWrite(LED_BUILTIN, state ? HIGH : LOW);
+    _application->runOneIteration();
 
-        if (state) {
-            std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-            uint64_t t = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count();
-            Serial.printf("******** now = %d\n", (int) t);
-        }
-    }
+    // if (ready) {
+    //     ready = false;
+    //     state = !state;
+    //     digitalWrite(LED_BUILTIN, state ? HIGH : LOW);
+
+    //     if (state) {
+    //         std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+    //         uint64_t t = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch()).count();
+    //         Serial.printf("******** now = %d\n", (int) t);
+    //     }
+    // }
 }
