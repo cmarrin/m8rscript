41b9dabed0ee81897c6b3238838b3e6f61bfb781
Chris Marrin <chris@marrin.com>
2020-04-11 15:49:27 -0700

    Added filesystem testing for Esp

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,48 @@ void setup()
 #endif    
 
     _application = new m8r::Application(23);
+
+    // Test filesystem
+    Serial.println("\n\nTesting Filesystem:");
+    m8r::String toPath("/foo");
+    m8r::Mad<m8r::File> toFile(m8r::system()->fileSystem()->open(toPath.c_str(), m8r::FS::FileOpenMode::Write));
+
+    if (!toFile->valid()) {
+        Serial.printf("Error: unable to open '%s' for write - ", toPath.c_str());
+        m8r::Error::showError(toFile->error());
+        Serial.printf("\n");
+    } else {
+        toFile->write("Hello World", 11);
+        if (!toFile->valid()) {
+            Serial.printf("Error writing '%s' - ", toPath.c_str());
+            m8r::Error::showError(toFile->error());
+            Serial.printf("\n");
+        } else {
+            Serial.printf("Successfully wrote '%s'\n", toPath.c_str());
+        }
+        toFile->close();
+    }
+
+    toFile = m8r::Mad<m8r::File>(m8r::system()->fileSystem()->open(toPath.c_str(), m8r::FS::FileOpenMode::Read));
+    if (!toFile->valid()) {
+        Serial.printf("Error: unable to open '%s' for read - ", toPath.c_str());
+        m8r::Error::showError(toFile->error());
+        Serial.printf("\n");
+    } else {
+        char buf[12];
+        int32_t result = toFile->read(buf, 11);
+        if (!toFile->valid()) {
+            Serial.printf("Error reading '%s' - ", toPath.c_str());
+            m8r::Error::showError(toFile->error());
+            Serial.printf("\n");
+        } else if (result != 11) {
+            Serial.printf("Wrong number of bytes read from '%s', expected 11, got %d\n", toPath.c_str(), result);
+        } else {
+            buf[11] = '\0';
+            Serial.printf("Successfully read '%s' - '%s'\n", toPath.c_str(), buf);
+        }
+        toFile->close();
+    }
 }
 
 void loop()
