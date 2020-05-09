3871cf4164e18615e6bdcd2fdf9b21efd2993fbd
Chris Marrin <chris@marrin.com>
2020-05-09 16:55:45 -0700

    Added new wSimulator target

---
+++ mac/Simulator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simulator
+//
+//  Created by Chris Marrin on 5/9/20.
+//  Copyright © 2020 MarrinTech. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
