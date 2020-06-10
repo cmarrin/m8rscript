b37c685febb3d2fc34ad30d1858752db963e3583
Chris Marrin <chris@marrin.com>
2020-06-10 06:34:37 -0700

    Switched back to hello.m8r

--- lib/m8rscript/src/Application.cpp
+++ lib/m8rscript/src/Application.cpp
@@ -104,7 +104,7 @@ Application::NameValidationType Application::validateBonjourName(const char* nam
 m8r::String Application::autostartFilename() const
 {
     // Look for it in config first
-    return "/sys/bin/mrsh";
+    return "/sys/bin/hello.m8r";
 }
 
 bool Application::mountFileSystem()
