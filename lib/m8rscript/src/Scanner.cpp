07c469261eaee1db694b0e6e6f2fd9b6b5d2feb8
Chris Marrin <chris@marrin.com>
2020-06-29 07:05:46 -0700

    Fixed a bug in handling "/="

--- lib/m8rscript/src/Scanner.cpp
+++ lib/m8rscript/src/Scanner.cpp
@@ -435,6 +435,12 @@ Token Scanner::scanComment()
 		}
 		return Token::Comment;
 	}
+
+    // This is either Slash or DIVSTO
+    if (c == '=') {
+        return Token::DIVSTO;
+    }
+    putback(c);
 	return Token::Slash;
 }
 
