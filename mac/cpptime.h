3d9389ceff1053b4a164551a62d965605fe6bbc9
Chris Marrin <chris@marrin.com>
2020-08-16 16:48:42 -0700

    Added cpptime.h from https://github.com/eglimi/cpptime for Mac timers

---
+++ mac/cpptime.h
@@ -0,0 +1,13 @@
+//
+//  cpptime.h
+//  m8rscript
+//
+//  Created by Chris Marrin on 8/16/20.
+//  Copyright © 2020 MarrinTech. All rights reserved.
+//
+
+#ifndef cpptime_h
+#define cpptime_h
+
+
+#endif /* cpptime_h */
