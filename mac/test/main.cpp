3fd8100e1d49f4fe5539b17b602590e229436b66
Chris Marrin <chris@marrin.com>
2020-11-17 15:51:03 -0800

    Changed test to use Vector for upload file names

--- mac/test/main.cpp
+++ mac/test/main.cpp
@@ -19,7 +19,7 @@
 
 m8rscript::M8rscriptScriptingLanguage m8rscriptScriptingLanguage;
 
-const char* fileList[] = {
+m8r::Vector<const char*> fileList = {
     "scripts/mem.m8r",
     "scripts/mrsh.m8r",
     "scripts/examples/NTPClient.m8r",
@@ -51,18 +51,15 @@ int main(int argc, char * argv[])
     m8r::system()->registerScriptingLanguage(&m8rscriptScriptingLanguage);
     
     // Upload files if present
-    int count = sizeof(fileList) / sizeof(const char*);
     const char* uploadPath = "/sys/bin";
     
-    for (int i = 0; i < count; ++i) {
-        const char* uploadFilename = fileList[i];
-
+    for (const auto it : fileList) {
         m8r::String toPath;
-        FILE* fromFile = fopen(uploadFilename, "r");
+        FILE* fromFile = fopen(it, "r");
         if (!fromFile) {
-            fprintf(stderr, "Unable to open '%s' for upload, skipping\n", uploadFilename);
+            fprintf(stderr, "Unable to open '%s' for upload, skipping\n", it);
         } else if (m8r::system()->fileSystem()) {
-            m8r::Vector<m8r::String> parts = m8r::String(uploadFilename).split("/");
+            m8r::Vector<m8r::String> parts = m8r::String(it).split("/");
             m8r::String baseName = parts[parts.size() - 1];
             
             if (uploadPath[0] != '/') {
@@ -92,7 +89,7 @@ int main(int argc, char * argv[])
                         size_t size = fread(&c, 1, 1, fromFile);
                         if (size != 1) {
                             if (!feof(fromFile)) {
-                                fprintf(stderr, "Error reading '%s', upload failed\n", uploadFilename);
+                                fprintf(stderr, "Error reading '%s', upload failed\n", it);
                                 success = false;
                             }
                             break;
@@ -107,7 +104,7 @@ int main(int argc, char * argv[])
                     }
                     toFile->close();
                     if (success) {
-                        printf("Uploaded '%s' to '%s'\n", uploadFilename, toPath.c_str());
+                        printf("Uploaded '%s' to '%s'\n", it, toPath.c_str());
                     }
                 }
             }
